9b0c4cc393c3da93176020eab40931974e3e8f9d
natronpc <nathan@autismcandles.com>
2020-12-05 14:58:49 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const xhr = new XMLHttpRequest();
+/*const express = require('express');
+const bodyParser = require('body-parser');
 
-var LoremIpsum = require('lorem-ipsum').LoremIpsum;
+const app = express();
 
-var lorem = new LoremIpsum({
-  sentencesPerParagraph: {
-    max: 8,
-    min: 4
-  },
-  wordsPerSentence: {
-    max: 16,
-    min: 4
-  }
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/server', (req, res) => {
+    document.getElementById('test').innerText = `${req.body}`;
 });
 
-app.get('/', (req, res) => res.send(lorem.generateParagraphs(7)))
+const port = 8080;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+*/
+var form = document.querySelector('.message-form')
+form.addEventListener('submit', auth);
+function auth() {
+    var in1 = document.querySelector('.message-form__input1')
+    var in2 = document.querySelector('.message-form__input2')
+    xhr.open('POST', '/server', true);
+    xhr.setRequestHeader('Content-Type', 'text')
+    xhr.send(`${in1.value}/${in2.value}`)
+}
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const net = require('net');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.post('/server', (req, res) => {
+	var sock = new net.Socket();
+	f(req.body.username, req.body.password);
+	console.log(`${req.body.username}/${req.body.password}`);
+
+	sock.on('data', function (data) {
+		res.send('sup nerd');
+		console.log('Received: ' + data);
+		sock.destroy(); // kill sock after server's response
+	});
+
+	sock.on('close', function () {
+		console.log('Connection closed');
+	});
+
+	function f(username, password) {
+		sock.connect(5150, '178.128.3.152', function () {
+			console.log('Connected');
+			sock.write(`!NEWUSER!::${username}*!/!*${password}`);
+		});
+	}
+
+});
+
+
+const port = 8080;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
